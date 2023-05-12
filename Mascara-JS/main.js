8d670d036c0fb7d905489b661c5b9a7b8256e842
pnauj <torres.jpw@gmail.com>
2023-05-12 13:04:01 -0300

    se finalizo el proyecto de mascaras

--- Mascara-JS/main.js
+++ Mascara-JS/main.js
@@ -2,7 +2,7 @@ const inputCard = document.querySelector("#inputCard");
 const inputDate = document.querySelector("#inputDate");
 const inputCVV = document.querySelector("#inputCVV");
 
-const maskNumber = "###-###-###-###";
+const maskNumber = "####-####-####-####";
 const maskDate = "##/##";
 const maskCVV = "###";
 
@@ -11,7 +11,7 @@ let cardNumber = [];
 let dateNumber = [];
 let cvvNumber = [];
 
-inputCard.addEventListener("Keydown", (e) => {
+inputCard.addEventListener("keydown", (e) => {
   if (e.key === "Tab") {
     return;
   }
@@ -19,6 +19,26 @@ inputCard.addEventListener("Keydown", (e) => {
   e.preventDefault();
   handleInput(maskNumber, e.key, cardNumber);
   inputCard.value = cardNumber.join("");
+});
+
+inputDate.addEventListener("keydown", (e) => {
+  if (e.key === "Tab") {
+    return;
+  }
+
+  e.preventDefault();
+  handleInput(maskDate, e.key, dateNumber);
+  inputDate.value = dateNumber.join("");
+});
+
+inputCVV.addEventListener("keydown", (e) => {
+  if (e.key === "Tab") {
+    return;
+  }
+
+  e.preventDefault();
+  handleInput(maskCVV, e.key, cvvNumber);
+  inputCVV.value = cvvNumber.join("");
 });
 
 function handleInput(mask, key, arr) {
@@ -30,10 +50,10 @@ function handleInput(mask, key, arr) {
   }
 
   if (numbers.includes(key) && arr.length + 1 <= mask.length) {
-    if(mask[arr.length] === '-' || mask[arr.length] === '/'){
-        arr.push(mask[arr.length], key)
+    if (mask[arr.length] === "-" || mask[arr.length] === "/") {
+      arr.push(mask[arr.length], key);
     } else {
-        arr.push(key)
+      arr.push(key);
     }
   }
 }
