9d55b39a05935cb4e5500e8dc6d4a01370d70737
pnauj <torres.jpw@gmail.com>
2023-05-20 02:46:47 -0300

    se finalizo el proyecto de json a csv

--- JSON to CSV/main.js
+++ JSON to CSV/main.js
@@ -1,3 +1,48 @@
-const jsonform = document.querySelector('#jsonform')
-const csvform = document.querySelector('#csvform')
-const bConvert = document.querySelector('#bConvert')
+const jsonform = document.querySelector("#jsonform");
+const csvForm = document.querySelector("#csvform");
+const bConvert = document.querySelector("#bConvert");
+
+bConvert.addEventListener("click", (e) => {
+  convertJSONtoCSV();
+});
+
+function convertJSONtoCSV() {
+  let json;
+  let keys = [];
+  let values = [];
+  try {
+    json = JSON.parse(jsonform.value);
+  } catch (error) {
+    console.log("Formato incorrecto JSON", error);
+    alert("Formato incorrecto JSON");
+  }
+
+  if (Array.isArray(json)) {
+    json.forEach((item) => {
+      const nKeys = Object.keys(item);
+
+      if (keys.length === 0) {
+        keys = [...nKeys];
+      } else {
+        if (nKeys.length != keys.length) {
+          throw new Error("Number of keys are different");
+        } else {
+          console.log("OK", nKeys);
+        }
+      }
+
+      const row = keys.map((k) => {
+        return item[k];
+      });
+
+      values.push([...row]);
+    });
+    console.log(keys, values);
+    values.unshift(keys);
+    const text = values.map((v) => v.join(",")).join("\n");
+    csvForm.value = text;
+
+  } else {
+    alert("no es un arreglo de objetos");
+  }
+}
